8adee97429ea4ec4fbb8a20a8c404a4f741f6a14
Ruben Polak <rubenpolak@wcw-stud-145-109-60-129.wireless.uva.nl>
2018-01-27 12:26:29 +0100

    Added NDCG@10 and Recall@1000

--- assignment_evals.sh
+++ assignment_evals.sh
@@ -14,11 +14,18 @@ for file in run/*; do
     fi
 
     #./trec_eval/trec_eval -q ap_88_89/qrel_test run/TF-IDF.run | grep -E "^map\s"
+
     echo $(./trec_eval/trec_eval "$arg0" "$qrel" "$file") >> temp.txt
-    xargs -n3 < temp.txt >> $filename
+    xargs -n3 < temp.txt >> $filename.temp
     cp /dev/null temp.txt
-    grep -E "^map\s|^P_5\s" $filename >> $filename.temp
-    mv $filename.temp $filename
+    grep -E "^map\s|^P_5\s|^num_rel\s|^num_rel_ret\s" $filename.temp >> $filename
+    cp /dev/null $filename.temp
+
+    # Adding additional non-default TREC measurements...
+    echo $(./trec_eval/trec_eval "-q" "-m" "ndcg_cut.10" "$qrel" "$file") >> $filename.temp
+    echo $(./trec_eval/trec_eval "-q" "-m" "recall.1000" "$qrel" "$file") >> $filename.temp
+    xargs -n3 < $filename.temp >> $filename
+    rm $filename.temp
 
 done
 rm temp.txt
